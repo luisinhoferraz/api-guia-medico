f029bfa591665aa94033984e23db4f102306d56f
luisinhoferraz <ferrazluisinho@gmail.com>
2018-05-11 14:24:04 -0300

    Indentação do código

--- api.php
+++ api.php
@@ -3,26 +3,19 @@
         <title>PHP Test</title>
     </head>
     <body>
-           <?php
-
-
-
-$conn = oci_connect('tasy', 'aloisk', '10.10.0.24:1521/tasy');
-$query = 'select DS_PLANO from pls_plano';
-$stid = oci_parse($conn, $query);
-oci_execute($stid, OCI_DEFAULT);
-while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
-foreach ($row as $item) {
-echo $item." | ";
-}
-echo "
-\n";
-}
-oci_free_statement($stid);
-oci_close($conn);
-
-
-
-?>
+        <?php
+            $conn = oci_connect('tasy', 'aloisk', '10.10.0.24:1521/tasy');
+            $query = 'select DS_PLANO from pls_plano';
+            $stid = oci_parse($conn, $query);
+            oci_execute($stid, OCI_DEFAULT);
+            while ($row = oci_fetch_array($stid, OCI_ASSOC)) {
+                foreach ($row as $item) {
+                    echo $item." | ";
+                }
+                echo "\n";
+            }
+            oci_free_statement($stid);
+            oci_close($conn);
+        ?>
     </body>
 </html>
