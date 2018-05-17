e207986372414f70b026d989eb59c691972782fa
luisinhoferraz <ferrazluisinho@gmail.com>
2018-05-17 11:03:10 -0300

    Planos de saúde obtidos e código comentado.

--- api.php
+++ api.php
@@ -1,26 +1,27 @@
 <html>
     <head>
-        <title>TESTE PHP</title>
+        <title>PLANOS DE SAÚDE</title>
         
     </head>
     <body>
         <?php
             $conn = oci_connect('tasy', 'aloisk', '10.10.0.24:1521/tasy');
-            $query = 'select DS_PLANO from pls_plano';
+
+            // a tabela de planos no banco de dados contém o código antigo e o código novo de cada plano registrado na ANS; porém, cada tupla possui apenas um dos dois códigos
+            // portanto a consulta PL/SQL executa o UNION, para considerar os dois códigos como uma coluna só e ao mesmo tempo ignorar as colunas vazias
+            
+            $query = 'select NR_PROTOCOLO_ANS, DS_PLANO from pls_plano where NR_PROTOCOLO_ANS is not null UNION select CD_SCPA, DS_PLANO from pls_plano where CD_SCPA is not null';
+            
             $stid = oci_parse($conn, $query);
             oci_execute($stid, OCI_DEFAULT);
+            
             while ($row = oci_fetch_array($stid, OCI_ASSOC)) { // percorre todas as linhas do resultado da consulta SQL
-                foreach ($row as $item) {
-                    $array[] = $item;
-                }
+                $array[] = ['codigo_ans' => $row['NR_PROTOCOLO_ANS'], 'nome_plano' => $row['DS_PLANO']]; // atribui os valores de cada plano a um array PHP
             }
 
-            print_r($array);
+            $json_str = json_encode($array); // transforma a variável PHP num array JSON
 
-            echo '<br /> FIM DO ARRAY PHP E INÍCIO DO ARRAY JSON <br />';
-
-            $json_str = json_encode($array);
-            echo 'JSON'.$json_str;
+            echo $json_str;
 
             oci_free_statement($stid);
             oci_close($conn);
