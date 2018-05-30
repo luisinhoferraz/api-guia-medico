ef56e39488a4b8cd1b776f6fa1e611a06aaef388
luisinhoferraz <ferrazluisinho@gmail.com>
2018-05-30 15:46:30 -0300

    Impressão dos registros separadamente ou em grupo

--- print.js
+++ print.js
@@ -1,7 +1,15 @@
-function printContent(el){
-    var printContents = document.getElementById(el).innerHTML;
+function printContent(e){
+    var printContents = document.getElementById(e).innerHTML;
     var originalContents = document.body.innerHTML;
     document.body.innerHTML = printContents;
     window.print();
     document.body.innerHTML = originalContents;
+}
+
+function narrowList(e){
+    $('.'+e).attr("class","impressao");
+}
+
+function returnList(e){
+    $('.'+e).attr("class","lista");
 }
